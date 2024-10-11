44f6b22fb7cf04468037346e1cb83bd2055c5be6
huaiyu.zuo <huaiyu.zuo@siger-data.com>
2024-10-11 10:01:24 +0800

    增加了x/y/z/rmse四个实时数据的动态显示

--- gui/overview.py
+++ gui/overview.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QComboBox
 from PyQt5.QtCore import QTimer
 import pyqtgraph as pg
 import random
@@ -10,6 +10,12 @@ class DynamicPlot(QWidget):
     def __init__(self, mqtt):
         super().__init__()
         self.setWindowTitle('碰撞保护实时数据图')
+        self.topic = "lrc/sensor/x"
+
+        # 创建下拉框
+        self.comboBox = QComboBox()
+        self.comboBox.addItems(['X轴数据', 'Y轴数据', 'Z轴数据', 'RMSE数据'])
+        self.comboBox.currentIndexChanged.connect(self.change_plot_type)
 
         # 创建PlotWidget实例
         self.plotWidget = pg.PlotWidget()
@@ -35,13 +41,32 @@ class DynamicPlot(QWidget):
 
         # 布局
         layout = QVBoxLayout()
+        layout.addWidget(self.comboBox)
         layout.addWidget(self.plotWidget)
         self.setLayout(layout)
         self.mqtt = mqtt
 
+    def change_plot_type(self, index):
+        # 根据下拉框的索引选择不同的数据源
+        if index == 0:
+            self.topic = "lrc/sensor/x"
+        elif index == 1:
+            self.topic = "lrc/sensor/y"
+        elif index == 2:
+            self.topic = "lrc/sensor/z"
+        elif index == 3:
+            self.topic = "lrc/sensor/r"
+        else:
+            self.topic = "lrc/sensor/x"
+        self.data.clear()
+        self.data = [0] * 100
+        for i in range(100):
+            self.data.append(self.mqtt.queues[self.topic].get())
+        self.plot.setData(self.data)
+
     def update_plot(self):
         # 更新数据
-        self.data.append(self.mqtt.queues["lrc/sensor/x"].get())
+        self.data.append(self.mqtt.queues[self.topic].get())
         if len(self.data) > 100:
             self.data.pop(0)
 
