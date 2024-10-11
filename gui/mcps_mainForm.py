c1b13b85485b9975db728df8c265da0dc782972e
karman-amber <karman.amber76@gmail.com>
2024-10-11 23:14:48 +0800

    调整为模块，不再支持直接运行

--- gui/mcps_mainForm.py
+++ gui/mcps_mainForm.py
@@ -159,12 +159,12 @@ class MainWindow(QMainWindow):
         self.statusBar().showMessage('显示关于信息')
 
 
-def main():
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     app = QApplication(sys.argv)
+#     main_window = MainWindow()
+#     main_window.show()
+#     sys.exit(app.exec_())
+#
+#
+# if __name__ == '__main__':
+#     main()
