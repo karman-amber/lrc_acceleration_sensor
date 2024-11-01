b2680dd5537065b1eccf350563e1f1e8136458e1
huaiyu.zuo <huaiyu.zuo@siger-data.com>
2024-11-01 14:46:30 +0800

    将多个监控曲线图合并成一个窗体内

--- gui/overview.py
+++ gui/overview.py
@@ -1,4 +1,3 @@
-
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox
 from PyQt5.QtCore import QTimer
 import pyqtgraph as pg
@@ -7,26 +6,46 @@ import pyqtgraph as pg
 class DynamicPlot(QWidget):
     def __init__(self, mqtt):
         super().__init__()
-        self.data = [0.0] * 1
+        self.data = {"x": [0.0], "y": [0.0], "z": [0.0]}
         self.setWindowTitle('碰撞保护实时数据图')
-        self.topic = "lrc/sensor/x"
+        self.topic = {"x": "lrc/sensor/x", "y": "lrc/sensor/y", "z": "lrc/sensor/z"}
         self.mqtt = mqtt
-        self.max_data = 0
-        self.min_data = 0
+        self.max_value = {"x": 0.0, "y": 0.0, "z": 0.0}
+        self.plots = dict()
 
         # 创建下拉框
-        self.comboBox = QComboBox()
-        self.comboBox.addItems(['X轴数据', 'Y轴数据', 'Z轴数据', 'RMSE数据'])
-        self.comboBox.currentIndexChanged.connect(self.change_plot_type)
+        # self.comboBox = QComboBox()
+        # self.comboBox.addItems(['X轴数据', 'Y轴数据', 'Z轴数据', 'RMSE数据'])
+        # self.comboBox.currentIndexChanged.connect(self.change_plot_type)
 
         # 创建PlotWidget实例
-        self.plotWidget = pg.PlotWidget()
-        self.plotWidget.setBackground('black')  # 设置背景颜色为黑色
-
-        # 绘制初始曲线
-        self.plot = self.plotWidget.plot(self.data, pen='yellow')
-        # 初始化数据
-        self.change_topic("x")
+        self.plotWidgets = {"x": pg.PlotWidget(), "y": pg.PlotWidget(), "z": pg.PlotWidget()}
+        for key, widget in self.plotWidgets.items():
+            widget.setBackground('black')  # 设置背景颜色为黑色
+            self.plots[key] = widget.plot(self.data[key], pen='yellow')
+            widget.setLabel('left', f"{key}轴加速度")
+            widget.setLabel('bottom', "时间")
+            widget.setTitle(f"{key}轴实时数据曲线")
+            self.data[key].clear()
+            # if key not in self.mqtt.queues or self.mqtt.queues[self.topic[key]].empty():
+            #     widget.setTitle(f"{key}轴实时数据曲线-已暂停")
+            # else:
+            #     while not self.mqtt.queues[self.topic[key]].empty():
+            #         v = self.mqtt.queues[self.topic[key]].get()
+            #         self.data[key].append(float(v))
+            # self.plots[key].setData(self.data[key])
+        # # 绘制初始曲线
+        # self.plot = self.plotWidget.plot(self.data, pen='yellow')
+        # # 初始化数据
+        # self.change_topic("x")
+        #
+        # self.plotWidget_y = pg.PlotWidget()
+        # self.plotWidget_y.setBackground('black')  # 设置背景颜色为黑色
+        #
+        # # 绘制初始曲线
+        # self.plot_y = self.plotWidget_y.plot(self.data, pen='yellow')
+        # # 初始化数据
+        # # self.change_topic("x")
 
         # 创建定时器
         self.timer = QTimer()
@@ -35,62 +54,26 @@ class DynamicPlot(QWidget):
 
         # 布局
         layout = QVBoxLayout()
-        layout.addWidget(self.comboBox)
-        layout.addWidget(self.plotWidget)
+        # layout.addWidget(self.comboBox)
+        for key, widget in self.plotWidgets.items():
+            layout.addWidget(widget)
         self.setLayout(layout)
-
-    def change_plot_type(self, index):
-        # 根据下拉框的索引选择不同的数据源
-        if index == 0:
-            self.change_topic("x")
-        elif index == 1:
-            self.change_topic("y")
-        elif index == 2:
-            self.change_topic("z")
-        elif index == 3:
-            self.change_topic("r")
-        else:
-            self.change_topic("x")
-
-    def change_topic(self, topic):
-        self.topic = "lrc/sensor/" + topic
-        self.plotWidget.setLabel('left', f"{topic}轴加速度")
-        self.plotWidget.setLabel('bottom', "时间")
-        self.plotWidget.setTitle(f"{topic}轴实时数据曲线")
-        # self.plotWidget.plotItem.getAxis('left').setPen('yellow')
-        # self.plotWidget.plotItem.getAxis('left').setLabel
-        # self.plotWidget.plotItem.getAxis('bottom').setPen('yellow')
-        self.data.clear()
-        self.max_data = -50
-        self.min_data = 50
-        # self.data = [0] * 1
-        if self.topic not in self.mqtt.queues or self.mqtt.queues[self.topic].empty():
-            self.plotWidget.setTitle(f"{topic}轴实时数据曲线-已暂停")
-        else:
-            for i in range(100):
-                v = self.mqtt.queues[self.topic].get()
-                self.data.append(float(v))
-
-        self.plot.setData(self.data)
 
     def update_plot(self):
         # 更新数据
-        if self.topic not in self.mqtt.queues or self.mqtt.queues[self.topic].empty():
-            self.plotWidget.setTitle(f"{self.topic[-1:]}轴实时数据曲线-已暂停")
-        else:
-            count = self.mqtt.queues[self.topic].qsize()
-            if count > 1:
-                data = 0
-                for i in range(count):
-                    data = float(self.mqtt.queues[self.topic].get(block=False))
-                    if data > self.max_data:
-                        self.max_data = data
-                    if data < self.min_data:
-                        self.min_data = data
-                    self.data.append(data)
-                self.plotWidget.setTitle(f"{self.topic[-1:]}轴实时数据曲线({round(self.min_data, 2)}至{round(self.max_data, 2)})")
-        if len(self.data) > 2000:
-            self.data = self.data[-2000:]
+        for key, widget in self.plotWidgets.items():
+            topic = self.topic[key]
+            if topic not in self.mqtt.queues or self.mqtt.queues[topic].empty():
+                widget.setTitle(f"{key}轴实时数据曲线-已暂停")
+            else:
+                count = self.mqtt.queues[topic].qsize()
+                if count > 1:
+                    for i in range(count):
+                        data = float(self.mqtt.queues[topic].get(block=False))
+                        self.data[key].append(data)
+                    widget.setTitle(f"{key}轴实时数据曲线")
+            if len(self.data[key]) > 2000:
+                self.data[key] = self.data[key][-2000:]
+            # 更新曲线
+            self.plots[key].setData(self.data[key])
 
-        # 更新曲线
-        self.plot.setData(self.data)
